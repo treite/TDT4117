6d485e582ab5889658ee9dd8f66b01bd44524c9d
treite <tarjei.reite@gmail.com>
2023-10-19 17:28:47 +0200

    could not do the last part of the task

--- task4.py
+++ task4.py
@@ -4,6 +4,8 @@ import codecs
 import string
 from nltk.stem.porter import PorterStemmer
 import gensim
+import pickle
+
 
 def createParagraphList(file):
     """
@@ -31,8 +33,9 @@ def filtering(paragraphs, word):
     """
     result = []
     for p in paragraphs:
-        if word.lower() not in p.lower():
-                result.append(p)
+        if len(p) > 2:
+            if word.lower() not in p.lower():
+                    result.append(p)
     return result
 
 def tokenize(paragraphs):
@@ -66,14 +69,78 @@ def stemmer(paragraphs):
         stemmedParagraphs.append(stemmedWords)
     return stemmedParagraphs
 
+def stopword(file):
+    """
+    file: string
+    return: list(string)
+
+    tar inn et filnavn, åpner fila, og returnerer en liste med en streng av hvert ord
+    """
+    result = []
+    with codecs.open(file, "r", "utf-8") as f:
+        content = f.read()
+        stopwords = content.split(",")
+        for s in stopwords:
+            s = s.strip()
+            result.append(s)
+    return result
+
+def preprocessing(query, dictionary):
+    """
+    query: string
+    return: list(list(int, int))
+    """
+    queryList = [query]
+    tokenizedQuery = tokenize(queryList)
+    stemmedQuery = stemmer(tokenizedQuery)
+
+    corpus = []
+    for word in stemmedQuery: 
+        corpus.append(dictionary.doc2bow(word))
+    return corpus
+
+
 paragraphs = createParagraphList("pg3300.txt")
 filtered_paragraphs = filtering(paragraphs, "Gutenberg")
+
+"""
 tokenized_paragraphs = tokenize(filtered_paragraphs)
+with open('tokenized_paragraphs.pkl', 'wb') as file:
+    pickle.dump(tokenized_paragraphs, file)
 stemmed_paragraphs = stemmer(tokenized_paragraphs)
+with open('stemmed_paragraphs.pkl', 'wb') as file:
+    pickle.dump(stemmed_paragraphs, file)
+"""
+# Load tokenized data
+with open('tokenized_paragraphs.pkl', 'rb') as file:
+    tokenized_paragraphs = pickle.load(file)
+# Load stemmed data
+with open('stemmed_paragraphs.pkl', 'rb') as file:
+    stemmed_paragraphs = pickle.load(file)
+
+dictionary = gensim.corpora.Dictionary(stemmed_paragraphs)
+stopwords = stopword("stopwords.txt")
+stop_ids = [dictionary.token2id[stopword] for stopword in stopwords if stopword in dictionary.token2id]
+dictionary.filter_tokens(stop_ids)
+
+corpus = []
+for paragraph in stemmed_paragraphs: 
+    corpus.append(dictionary.doc2bow(paragraph))
+
+tfidf_model = gensim.models.TfidfModel(corpus)
+tfidf_corpus = tfidf_model[corpus]
+tfidf_index = gensim.similarities.MatrixSimilarity(corpus, dictionary)
+
+lsi_model = gensim.models.LsiModel(tfidf_corpus, id2word=dictionary, num_topics=100)
+lsi_corpus = lsi_model[tfidf_corpus]
+lsi_index = gensim.similarities.MatrixSimilarity(lsi_corpus, num_features=len(dictionary))
+
+#print(lsi_model.show_topics(3))
+
+query = preprocessing("What is the function of money?", dictionary)
+tfidf_query = tfidf_model[query][0]
 
 print(len(paragraphs)," ", len(filtered_paragraphs)," ", len(tokenized_paragraphs)," ", len(stemmed_paragraphs))
 
-
-print(1)
 
 
